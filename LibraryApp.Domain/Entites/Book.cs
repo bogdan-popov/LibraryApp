96642b6507dec5b8917b72e6a68c0f525b877e7f
bogdan-popov <bogdanpopov5570@gmail.com>
2025-09-26 21:27:19 +0300

    feat(domain): Add core entities User, Book, Subscription

---
+++ LibraryApp.Domain/Entites/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Domain.Entites;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public int? BorrowedByUserId { get; set; }
+    public User? BorrowedByUser { get; set; }
+}
